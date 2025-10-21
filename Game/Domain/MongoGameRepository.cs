46874e1f2bf802a8f9e34f85491db1ecef067dd1
MrPlesk2 <plesk9@mail.ru>
2025-10-21 15:51:57 +0500

    task 3

--- Game/Domain/MongoGameRepository.cs
+++ Game/Domain/MongoGameRepository.cs
@@ -1,45 +1,68 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using MongoDB.Driver;
 
 namespace Game.Domain
 {
-    // TODO Сделать по аналогии с MongoUserRepository
     public class MongoGameRepository : IGameRepository
     {
+        private readonly IMongoCollection<GameEntity> gameCollection;
         public const string CollectionName = "games";
 
         public MongoGameRepository(IMongoDatabase db)
         {
+            gameCollection = db.GetCollection<GameEntity>(CollectionName);
+
+            var indexKeys = Builders<GameEntity>.IndexKeys.Ascending(g => g.Status);
+            var indexModel = new CreateIndexModel<GameEntity>(indexKeys);
+            gameCollection.Indexes.CreateOne(indexModel);
         }
 
         public GameEntity Insert(GameEntity game)
         {
-            throw new NotImplementedException();
+            if (game.Id == Guid.Empty)
+            {
+                var idField = typeof(GameEntity).GetProperty("Id");
+                idField?.SetValue(game, Guid.NewGuid());
+            }
+
+            gameCollection.InsertOne(game);
+            return game;
         }
+
 
         public GameEntity FindById(Guid gameId)
         {
-            throw new NotImplementedException();
+            return gameCollection.Find(g => g.Id == gameId).FirstOrDefault();
         }
 
         public void Update(GameEntity game)
         {
-            throw new NotImplementedException();
+            gameCollection.ReplaceOne(g => g.Id == game.Id, game);
         }
 
-        // Возвращает не более чем limit игр со статусом GameStatus.WaitingToStart
         public IList<GameEntity> FindWaitingToStart(int limit)
         {
-            //TODO: Используй Find и Limit
-            throw new NotImplementedException();
+            return gameCollection
+                .Find(g => g.Status == GameStatus.WaitingToStart)
+                .Sort(Builders<GameEntity>.Sort.Ascending("_id"))
+                .Limit(limit)
+                .ToList();
         }
 
-        // Обновляет игру, если она находится в статусе GameStatus.WaitingToStart
+
+
         public bool TryUpdateWaitingToStart(GameEntity game)
         {
-            //TODO: Для проверки успешности используй IsAcknowledged и ModifiedCount из результата
-            throw new NotImplementedException();
+            var filter = Builders<GameEntity>.Filter.And(
+                Builders<GameEntity>.Filter.Eq(g => g.Id, game.Id),
+                Builders<GameEntity>.Filter.Eq(g => g.Status, GameStatus.WaitingToStart)
+            );
+
+            var result = gameCollection.ReplaceOne(filter, game);
+
+            return result.IsAcknowledged && result.ModifiedCount > 0;
         }
     }
 }
