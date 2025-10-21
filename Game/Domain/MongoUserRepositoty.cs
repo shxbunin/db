90e8be383c8fa9b965ed8fd4845f7608ce9dc98d
Igor Shabunin <shabunin181104@gmail.com>
2025-10-21 15:18:59 +0500

    task 2

--- Game/Domain/MongoUserRepositoty.cs
+++ Game/Domain/MongoUserRepositoty.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using MongoDB.Driver;
 
 namespace Game.Domain
@@ -11,46 +12,71 @@ namespace Game.Domain
         public MongoUserRepository(IMongoDatabase database)
         {
             userCollection = database.GetCollection<UserEntity>(CollectionName);
+            
+            var indexKeys = Builders<UserEntity>.IndexKeys.Ascending(u => u.Login);
+            var indexOptions = new CreateIndexOptions { Unique = true, Name = "unique_login" };
+            var indexModel = new CreateIndexModel<UserEntity>(indexKeys, indexOptions);
+            userCollection.Indexes.CreateOne(indexModel);
         }
 
         public UserEntity Insert(UserEntity user)
         {
-            //TODO: Ищи в документации InsertXXX.
-            throw new NotImplementedException();
-        }
+            if (user.Id == Guid.Empty)
+                user = new UserEntity(Guid.NewGuid(), user.Login, user.LastName, user.FirstName, user.GamesPlayed, user.CurrentGameId);
 
+            userCollection.InsertOne(user);
+            return user;
+        }
+        
         public UserEntity FindById(Guid id)
         {
-            //TODO: Ищи в документации FindXXX
-            throw new NotImplementedException();
+            return userCollection.Find(u => u.Id == id).FirstOrDefault();
         }
-
+        
         public UserEntity GetOrCreateByLogin(string login)
         {
-            //TODO: Это Find или Insert
-            throw new NotImplementedException();
-        }
+            var existing = userCollection.Find(u => u.Login == login).FirstOrDefault();
+            if (existing != null)
+                return existing;
+            
+            var newUser = new UserEntity(Guid.NewGuid(), login, "", "", 0, null);
 
+            try
+            {
+                userCollection.InsertOne(newUser);
+                return newUser;
+            }
+            catch (MongoWriteException e) when (e.WriteError.Category == ServerErrorCategory.DuplicateKey)
+            {
+                return userCollection.Find(u => u.Login == login).FirstOrDefault();
+            }
+        }
+        
         public void Update(UserEntity user)
         {
-            //TODO: Ищи в документации ReplaceXXX
-            throw new NotImplementedException();
+            userCollection.ReplaceOne(u => u.Id == user.Id, user);
         }
-
+        
         public void Delete(Guid id)
         {
-            throw new NotImplementedException();
+            userCollection.DeleteOne(u => u.Id == id);
         }
-
-        // Для вывода списка всех пользователей (упорядоченных по логину)
-        // страницы нумеруются с единицы
+        
         public PageList<UserEntity> GetPage(int pageNumber, int pageSize)
         {
-            //TODO: Тебе понадобятся SortBy, Skip и Limit
-            throw new NotImplementedException();
+            var totalCount = userCollection.CountDocuments(FilterDefinition<UserEntity>.Empty);
+
+            var users = userCollection
+                .Find(FilterDefinition<UserEntity>.Empty)
+                .SortBy(u => u.Login)
+                .Skip((pageNumber - 1) * pageSize)
+                .Limit(pageSize)
+                .ToList();
+
+            return new PageList<UserEntity>(users, totalCount, pageNumber, pageSize);
         }
 
-        // Не нужно реализовывать этот метод
+        // Не требуется реализовывать
         public void UpdateOrInsert(UserEntity user, out bool isInserted)
         {
             throw new NotImplementedException();
